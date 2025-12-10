8ccd3f52fc66dab1e5b365f30a21581ee6348423
hoons <khg6644089@hanmail.net>
2025-12-10 11:00:07 +0900

    1210 hoons

--- src/main/java/com/study/spring/hospital/controller/MedicalController.java
+++ src/main/java/com/study/spring/hospital/controller/MedicalController.java
@@ -2,21 +2,22 @@ package com.study.spring.hospital.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.spring.hospital.dto.MedicalListDto;
-import com.study.spring.hospital.repository.MedicalReopository;
+import com.study.spring.hospital.service.MedicalService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 public class MedicalController {
 
-    @Autowired
-    MedicalReopository mReop;
+    private final MedicalService medicalService;
 
     @GetMapping("/api/appmlist")
     public List<MedicalListDto> getMedicalList() {
-        return mReop.findAllMedicalByIdDesc();
+        return medicalService.getMedicalList();
     }
 }
