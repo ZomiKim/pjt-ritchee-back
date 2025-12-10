d4876cf40979c5a6bcd67d12937ac756aa2483a3
lhn250700-byte <lhn250700@gmail.com>
2025-12-10 09:49:04 +0900

    1210 sky

--- src/main/java/com/study/spring/hospital/dto/HospitalSumDTO.java
+++ src/main/java/com/study/spring/hospital/dto/HospitalSumDTO.java
@@ -5,7 +5,6 @@ public interface HospitalSumDTO {
 	String getH_code();
 	String getH_addr();
 	String getH_tel1();
-	String getH_tel2();
 	String getH_park_yn();
 	String getH_bigo();
 	String getH_name();
--- src/main/java/com/study/spring/hospital/dto/HospitalSumInfoDTO.java
+++ src/main/java/com/study/spring/hospital/dto/HospitalSumInfoDTO.java
@@ -9,7 +9,6 @@ public interface HospitalSumInfoDTO {
 		String getH_smpl_dgm();
 		String getH_addr();
 		String getH_tel1();
-		String getH_tel2();
 		String getH_long();
 		String getH_lat();
 		String getH_park_yn();
